06ae40e323c813e9dc67c7a89d139465bc10cd70
suborgerry <borgerrywork@gmail.com>
2026-09-01 13:12:21 +0300

    V1

--- jotform-bridge/jotform-bridge.php
+++ jotform-bridge/jotform-bridge.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Jotform Bridge
  * Plugin URI:        https://github.com/suborgerry/jotform-bridge
  * Description:       Uses Jotform as a headless form backend for WordPress: custom markup on the site, submissions and data storage on Jotform.
- * Version:           0.2.1
+ * Version:           1.0.0
  * Requires at least: 6.4
  * Requires PHP:      8.0
  * Author:            Jotform Bridge
@@ -27,7 +27,7 @@ if (defined('JOTFORM_BRIDGE_VERSION')) {
     return;
 }
 
-define('JOTFORM_BRIDGE_VERSION', '0.2.1');
+define('JOTFORM_BRIDGE_VERSION', '1.0.0');
 define('JOTFORM_BRIDGE_FILE', __FILE__);
 define('JOTFORM_BRIDGE_DIR', plugin_dir_path(__FILE__));
 define('JOTFORM_BRIDGE_URL', plugin_dir_url(__FILE__));
--- jotform-bridge/readme.txt
+++ jotform-bridge/readme.txt
@@ -4,7 +4,7 @@ Tags: jotform, forms, contact form, headless, custom form
 Requires at least: 6.4
 Tested up to: 6.8
 Requires PHP: 8.0
-Stable tag: 0.2.1
+Stable tag: 1.0.0
 License: GPL-2.0-or-later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
